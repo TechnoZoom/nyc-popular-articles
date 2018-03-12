6385728e513dfa8146ea1d440f3840300e060080
technozoom <kapil.bakshi@blackbuck.com>
2018-03-12 09:57:55 +0530

    Api key made secured using NDK

--- app/src/main/java/mostpopular/kapil/com/nycmostpopular/api/MostPopularApis.java
+++ app/src/main/java/mostpopular/kapil/com/nycmostpopular/api/MostPopularApis.java
@@ -4,8 +4,10 @@ import io.reactivex.Flowable;
 import mostpopular.kapil.com.nycmostpopular.config.ServerEndPoints;
 import mostpopular.kapil.com.nycmostpopular.models.responses.AllArticlesResponse;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface MostPopularApis {
+
     @GET(ServerEndPoints.MOST_POPULAR_APIS_ARTICLES_LIST)
-    Flowable<AllArticlesResponse> getArticles();
+    Flowable<AllArticlesResponse> getArticles(@Query("api-key")String nycApiKey);
 }
